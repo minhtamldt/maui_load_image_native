af2cfbff2c304b6c7502d27b12aac1ad6ff32824
TamTM <minhtamldt@gmail.com>
2023-03-25 22:26:22 +0700

    load image native in maui

--- LoadingImageNative/LoadingImageNative/Views/MainPage.xaml.cs
+++ LoadingImageNative/LoadingImageNative/Views/MainPage.xaml.cs
@@ -1,10 +1,21 @@
-﻿namespace LoadingImageNative.Views;
+﻿
+namespace LoadingImageNative.Views;
 
 public partial class MainPage : ContentPage
 {
     public MainPage()
     {
         InitializeComponent();
+
+#if __IOS__
+        var imageNames = new List<string>();
+        var bundle = Foundation.NSBundle.MainBundle;
+
+        foreach (var imageName in Foundation.NSBundle.MainBundle.PathsForResources("png", null))
+        {
+            imageNames.Add(System.IO.Path.GetFileNameWithoutExtension(imageName));
+        }
+#endif 
     }
 }
 
